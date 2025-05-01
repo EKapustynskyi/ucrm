b22476509339b44eadb02efbc0c575610b4f184b
Yevgeniy Kapustynskyi <eugenekapustynskyi@gmail.com>
2025-05-01 12:44:24 +0300

    Add Tasks controller with CRUD

--- app/Http/Controllers/Api/V1/TasksController.php
+++ app/Http/Controllers/Api/V1/TasksController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTasksRequest;
 use App\Http\Requests\UpdateTasksRequest;
+use App\Http\Resources\TasksResource;
 use App\Models\Tasks;
+use Illuminate\Console\View\Components\Task;
+use Illuminate\Http\Request;
 
 class TasksController extends Controller
 {
@@ -14,15 +17,7 @@ class TasksController extends Controller
      */
     public function index()
     {
-        return Tasks::all();
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return TasksResource::collection(Tasks::all());
     }
 
     /**
@@ -30,38 +25,50 @@ class TasksController extends Controller
      */
     public function store(StoreTasksRequest $request)
     {
-        //
+        $tasks = Tasks::create($request->validated());
+        return TasksResource::make($tasks);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Tasks $tasks)
+    public function show($id): TasksResource
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Tasks $tasks)
-    {
-        //
+        $task = Tasks::find($id);
+        if (!$task) {
+            // Повертаємо API JSON 404
+            abort(response()->json(['message' => 'Task not found'], 404));
+        }
+        return TasksResource::make($task);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateTasksRequest $request, Tasks $tasks)
+    public function update(UpdateTasksRequest $request, $id)
     {
-        //
+//        $tasks = Tasks::update($request->validated());
+        $tasks = Tasks::find($id);
+        $tasks->update($request->validated());
+        return TasksResource::make($tasks);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Tasks $tasks)
+    public function destroy($id)
     {
-        //
+        $tasks = Tasks::find($id);
+        $tasks->delete();
+        $tasks->refresh();
+        return response()->noContent();
+    }
+
+    public function complete(Request $request, $id)
+    {
+        $tasks = Tasks::find($id);
+        $tasks->is_completed = $request->is_completed;
+        $tasks->save();
+        return TasksResource::make($tasks);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::apiResource('/tasks', TasksController::class);
+//    Route::apiResource('tasks', TasksController::class);
+    Route::get('/tasks', [TasksController::class, 'index']);
+    Route::post('/tasks', [TasksController::class, 'store']);
+    Route::get('/tasks/{id}', [TasksController::class, 'show']);
+    Route::put('/tasks/{id}', [TasksController::class, 'update']);
+    Route::delete('/tasks/{id}', [TasksController::class, 'destroy']);
+    Route::patch('/tasks/{id}', [TasksController::class, 'complete']);
+
 });
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/test-check', function () {
+    return response()->json(['ok' => true]);
+});
